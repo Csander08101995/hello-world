49cb267e70c291bebf2a1c0cbb247d4d3b8908a8
Csander08101995 <stepanovAlecsa@yandex.ru>
2016-04-01 10:28:32 +0300

    Lab3 commit

--- src/First.java
+++ src/First.java
@@ -1,8 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by Саня on 01.04.2016.
  */
 public class First {
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        Laba.main();
     }
+
+
 }
